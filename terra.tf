580da0b62beb2126c036b2520983ba3bfa46ead0
aleksandr <aleksandr_andreichenko@epam.com>
2018-08-20 14:17:13 +0300

    added provisioner inline command for start

--- terra.tf
+++ terra.tf
@@ -1,10 +1,17 @@
 provider "aws" {
-  access_key = "*********"
-  secret_key = "**********"
+  access_key = "******"
+  secret_key = "******"
   region = "eu-central-1"
 }
 
 resource "aws_instance" "test1" {
   ami = "ami-c86c3f23"
   instance_type = "t2.micro"
+
+  provisioner "remote-exec" {
+      inline = [
+        "sudo yum install nginx -y",
+        "sudo service nginx start"
+      ]
+  }
 }
