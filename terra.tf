9e2e9ca687c0776c62b8c314840f47b201b8fb3c
aleksandr <aleksandr_andreichenko@epam.com>
2018-08-20 13:52:14 +0300

    start test master aws

---
+++ terra.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "*********"
+  secret_key = "**********"
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "test1" {
+  ami = "ami-c86c3f23"
+  instance_type = "t2.micro"
+}
